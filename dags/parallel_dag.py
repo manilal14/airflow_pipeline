bd82f8ecaf10c4ef10844993f070ef0a66098074
mani <manihereatcusat@gmail.com>
2022-07-14 21:45:33 +0530

    sub dag is implemented

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,36 +1,35 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from subdag.subdag_parallel_dag import subdag_parallel_dag
  
 from datetime import datetime
+
+
+default_args = {
+    'start_date' : datetime(2022, 7, 13)
+}
  
-with DAG('parallel_dag', start_date=datetime(2022, 1, 1), 
-    schedule_interval='@daily', catchup=False) as dag:
+with DAG('parallel_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
  
     extract_a = BashOperator(
         task_id='extract_a',
-        bash_command='sleep 5'
+        bash_command='sleep 3'
     )
  
-    extract_b = BashOperator(
-        task_id='extract_b',
-        bash_command='sleep 1'
+    # extract_b = BashOperator(
+    #     task_id='extract_b',
+    #     bash_command='sleep 1'
+    # )
+
+    subdag_processing = SubDagOperator(
+        task_id = 'subdag_processing',
+        subdag = subdag_parallel_dag('parallel_dag','subdag_processing', default_args)
     )
- 
-    load_a = BashOperator(
-        task_id='load_a',
-        bash_command='sleep 1'
-    )
- 
-    load_b = BashOperator(
-        task_id='load_b',
-        bash_command='sleep 8'
-    )
- 
+
     transform = BashOperator(
         task_id='transform',
         bash_command='sleep 1'
     )
  
-    extract_a >> load_a
-    extract_b >> load_b
-    [load_a, load_b] >> transform
+    extract_a >> subdag_processing >> transform
